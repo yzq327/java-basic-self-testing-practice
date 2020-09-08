f63a6404202c5e44a06f4bfa9f58ab1b87ad52a4
yzq327 <69582746+yzq327@users.noreply.github.com>
2020-09-08 11:06:41 +0800

    Update GenericTest.java

--- src/test/java/com/twc/javaBasic/GenericTest.java
+++ src/test/java/com/twc/javaBasic/GenericTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class GenericTest {
+class GenericTest { //泛型
   @Test
   void should_auto_resolve_generic_method() {
     final String[] words = {"Hello", "Good", "Morning"};
@@ -14,21 +14,48 @@ class GenericTest {
     //  The getMiddle method is a generic method. Now, please call getMiddle method for string
     //  type.
     // <--start
-    final String middle = null;
-    // --end-->
+   // 里面的var类型为String类型
 
+    final String middle =  getMiddle(words);
+    // --end-->
     assertEquals("Good", middle);
+
   }
+  private static <T> T getMiddle(T[] args) {
+    return args[args.length / 2];
+  }
+
 
   @Test
   void should_specify_a_type_restriction_on_typed_parameters() {
     // Hint: please implement the min() method in this class.
     int minimumInteger = min(new Integer[] {1, 2, 3});
+    System.out.println(minimumInteger);
+
     double minimumReal = min(new Double[] {1.2, 2.2, -1d});
+    System.out.println(minimumInteger);
 
     assertEquals(1, minimumInteger);
     assertEquals(-1d, minimumReal, 1.0E-05);
   }
+  // TODO:
+  //  please implement the following code to pass the test. It should be generic
+  //  after all. The method should only accept `Number` and the number should
+  //  implement `Comparable<T>`.
+  //  You should not change the signature of the function. But you can change
+  //  the declaration of the generic type parameter.
+  // <--start
+  private static <T extends Comparable<T>> T min(T[] values) {
+    T min=values[0];
+    for (int i=0;i<values.length;i++){
+      if(min.compareTo(values[i])>0){
+          min=values[i];
+      }
+    }
+    return min;
+  }
+  // --end-->
+
 
   @Test
   void should_swap() {
@@ -41,22 +68,6 @@ class GenericTest {
     assertEquals("Hello", pair.getSecond());
   }
 
-  private static <T> T getMiddle(T[] args) {
-    return args[args.length / 2];
-  }
-
-  // TODO:
-  //  please implement the following code to pass the test. It should be generic
-  //  after all. The method should only accept `Number` and the number should
-  //  implement `Comparable<T>`.
-  //  You should not change the signature of the function. But you can change
-  //  the declaration of the generic type parameter.
-  // <--start
-  private static <T> T min(T[] values) {
-    throw new RuntimeException("Not implemented");
-  }
-  // --end-->
-
   // TODO:
   //  please implement following method to pass the test. The method should be able
   //  to swap fields in a pair. But you cannot change the signature of the function.
@@ -64,11 +75,21 @@ class GenericTest {
   // Hint:
   //  A wildcard is not a type variable, so we can’t write code that uses ? as a type.
   // <--start
-  private static void swap(Pair<?> pair) {
-    throw new RuntimeException("Not implemented");
+  private static void swap(Pair<String> pair) { //<?>泛型通配符
+    String temp;
+    temp=pair.getFirst();
+    pair.setFirst(pair.getSecond());
+    pair.setSecond(temp);
+
   }
+
+
+
+
 
   // TODO: You can add additional method within the range if you like
 
-  // --end-->
+  // --end--
+
+
 }
