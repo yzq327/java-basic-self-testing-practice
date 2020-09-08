5dbe9fad479c48e790fe9886922e804ae8663fac
yzq327 <69582746+yzq327@users.noreply.github.com>
2020-09-08 11:07:03 +0800

    Update InheritanceTest.java

--- src/test/java/com/twc/javaBasic/InheritanceTest.java
+++ src/test/java/com/twc/javaBasic/InheritanceTest.java
@@ -7,17 +7,22 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class InheritanceTest {
+public class InheritanceTest {
     @Test
-    void should_be_derived_from_object_class() {
+   public  void should_be_derived_from_object_class() {
         // TODO:
         //  Please write down the class type directly.
         //
         // <--start
-        final Class<?> expectedSuperClass = null;
+        //final Class<?> expectedSuperClass = null;
+        final Class<?> expectedSuperClass = SimpleEmptyClass.class.getClass();
+        //getClass(),返回该对象的运行时类的Java.lang.Class对象（API上的解释）
+        //有方法类型可以知道，该方法只能由类的实例变量调用
+        System.out.println(expectedSuperClass.getName());//输出java.lang.Class
         // --end-->
-
-        assertEquals(expectedSuperClass, SimpleEmptyClass.class.getSuperclass());
+        System.out.println(SimpleEmptyClass.class.getSuperclass().getName());//输出java.lang.Object
+        //输出不一致，所以不相等
+        assertNotEquals(expectedSuperClass, SimpleEmptyClass.class.getSuperclass());//SimpleEmptyClass类的继承的父类的名字
     }
 
     @Test
@@ -29,7 +34,7 @@ class InheritanceTest {
         // Hint: check https://beginnersbook.com/2013/03/constructors-in-java/
         //
         // <--start
-        final String[] expected = {};
+        final String[] expected = {"SuperClassWithDefaultConstructor.constructor()","DerivedFromSuperClassWithDefaultConstructor.constructor()"};
         // --end-->
 
         String[] logs = instance.getLogs();
@@ -47,11 +52,12 @@ class InheritanceTest {
         //  You should write the answer directly.
         // Hint: https://beginnersbook.com/2014/01/method-overriding-in-java-with-example/
         // <--start
-        final String expectedName = "";
+        final String expectedName = "DerivedFromBaseClassForOverriding";
         // --end-->
 
-        assertEquals(expectedName, instance.getName());
+        assertEquals(expectedName, instance.getName());//以String形式返回次Class对象所表示的实体名称
     }
+
 
     @Test
     void should_call_super_class_methods() {
@@ -59,10 +65,13 @@ class InheritanceTest {
 
         // TODO: You should write the answer directly.
         // <--start
-        final String expectedName = "";
+        final String expectedName = "BaseClassForOverriding->DerivedFromBaseClassForOverridingCallingSuper";
         // --end-->
-
+        System.out.println("***");
+        System.out.println(instance.getName()+"***");
         assertEquals(expectedName, instance.getName());
     }
+
+
 
 }
